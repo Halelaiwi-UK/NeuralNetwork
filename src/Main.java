f667375929721a9db730dbb3d4528147aeb7690c
BuildTools <unconfigured@null.spigotmc.org>
2024-09-30 12:03:19 +0100

    Forward feed done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import org.apache.commons.math3.linear.RealVector;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         NeuralNetwork network = new NeuralNetwork(2, 2);
